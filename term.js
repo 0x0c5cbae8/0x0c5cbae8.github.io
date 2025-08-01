8855267d4286da5d1e7dd02e7539aee3e17d53b8
0x0c5cbae8 <tsk.tsk.tsk.tsk.heh@gmail.com>
2025-08-01 22:26:03 +0800

    Revamped IO

--- term.js
+++ term.js
@@ -1,89 +1,131 @@
 // Copyleft (ɔ) 2025, Charlie Yang. All lefts reserved.
 
-class Shell {
+class IO {
     constructor() {
-        this.term = new Terminal({cursorBlink: true, fontSize: 24, theme: {background: '#1e1e2e', foreground: '#cdd6f4'}});
+        this.term = new Terminal({cursorBlink: true, fontSize: 24, theme: {background: '#1e1e2e', foreground: '#cdd6f4', cursor: '#cdd6f4'}});
         this.fitAddon = new FitAddon.FitAddon();
-        this.cwd = '[0x0c5cbae8 ~]$ ';
         this.buffer = '';
         this.cursor = 0;
-        this.promptMarker = 0;
+        this.cursorOffset = {x: 0, y: 0};
         this.history = [''];
         this.historyIndex = 0;
+        this.saveHistory = false;
+        this.inputResolver = null;
+        this.rejecter = null;
+        this.rejected = false;
 
         this.term.loadAddon(this.fitAddon);
         this.term.open(document.getElementById('terminal'));
         this.fitAddon.fit();
         this.term.onData(data => {
-            data = data.replace(/\r\n/g, '\r').replace(/\n/g, '\r');
-            const lines = data.split(/(\r)/);
-            lines.forEach(line => this.handleInput(line));
+            this.handleInput(data);
         });
-        this.term.write(this.cwd);
+    }
+
+    async print(output) {
+        return new Promise((resolve, reject) => {
+            if (this.rejected) {
+                this.rejected = false;
+                reject(new Error('killed'));
+                return;
+            }
+            this.term.write(output, () => {
+                this.setCursorOffset();
+                resolve();
+            });
+        });
+    }
+
+    async readLine(saveHistory = false) {
+        return new Promise((resolve, reject) => {
+            if (this.rejected) {
+                this.rejected = false;
+                reject(new Error('killed'));
+                return;
+            }
+            this.term.write('\x1b[?25h', () => {
+                this.saveHistory = saveHistory;
+                this.inputResolver = resolve;
+                this.rejecter = reject;
+            });
+        });
+    }
+
+    async sleep(ms) {
+        return new Promise((resolve, reject) => {
+            if (this.rejected) {
+                this.rejected = false;
+                reject(new Error('killed'));
+                return;
+            }
+            this.rejecter = reject;
+            setTimeout(() => {
+                this.rejecter = null;
+                resolve();
+            }, ms);
+        });
+    }
+
+    setCursorOffset() {
+        let y = this.term.buffer.active.cursorY + this.term.buffer.active.viewportY;
+        let x = this.term.buffer.active.cursorX;
+        if (x >= this.term.cols) {
+            y++;
+            x = 0;
+        }
+        this.cursorOffset = {x: x, y: y};
     }
 
     setCursorPosition(pos) {
-        pos += this.cwd.length;
-        const offset = this.promptMarker - this.term.buffer.active.viewportY;
-        const y = Math.floor(pos / this.term.cols) + offset;
-        const x = pos % this.term.cols;
+        let y = this.cursorOffset.y;
+        let x = this.cursorOffset.x + pos;
+        y += Math.floor(x / this.term.cols);
+        x = x % this.term.cols;
+        y -= this.term.buffer.active.viewportY;
+
         if (y==this.term.rows) {
             return`\x1b[${y};${x + 1}H\n`;
         }
         return `\x1b[${y + 1};${x + 1}H`;
     }
 
-    rewriteLine(len) {
-        let output = this.setCursorPosition(-this.cwd.length);
-        const numLines = Math.ceil((this.cwd.length + len) / this.term.cols);
-        for (let i = 0; i < numLines; i++) {
-            output += '\x1b[2K';
-            if (i < numLines - 1) output += '\n';
-        }
-        output += this.setCursorPosition(-this.cwd.length) + this.cwd + this.buffer;
+    rewriteInput() {
+        let output = this.setCursorPosition(0);
+        output += '\x1b[0J';
+        output += this.buffer;
         output += this.setCursorPosition(this.cursor);
         return output;
     }
 
     handleReturnKey() {
-        if(this.buffer.trim() !== '') {
+        if(this.buffer.trim() !== '' && this.saveHistory) {
             this.history[this.history.length-1] = this.buffer;
             this.historyIndex = this.history.length;
             this.history.push('');
         }
-        let output = this.setCursorPosition(this.buffer.length - 1) + '\r\n';
-        output += this.runCommand(this.buffer);
-        output += this.cwd;
-        let newMarker = this.promptMarker;
-        newMarker += (output.match(/\n/g)||[]).length;
-        const lines = output.split('\r\n');
-        newMarker += Math.ceil((this.cwd.length + this.buffer.length) / this.term.cols) - 1;
-        for (let i = 0; i < lines.length; i++) {
-            newMarker += Math.max(0, Math.ceil(lines[i].length / this.term.cols)-1);
-        }
-        this.buffer = '';
-        this.cursor = 0;
-        this.promptMarker = newMarker;
-        this.term.write(output, () => {
-            this.promptMarker = this.term.buffer.active.cursorY+this.term.buffer.active.viewportY;
-        });
+        return this.setCursorPosition(Math.max(0, this.buffer.length - 1)) + '\r\n';
     }
 
     handleLeftKey() {
         if (this.cursor > 0) {
             this.cursor--;
-            this.term.write('\x1b[D');
+            return this.setCursorPosition(this.cursor);
         }
+        return '';
     }
 
     handleRightKey() {
         if (this.cursor < this.buffer.length) {
             this.cursor++;
-            this.term.write('\x1b[C');
+            return this.setCursorPosition(this.cursor);
         }
+        return '';
     }
 
     handleUpKey() {
+        if (!this.saveHistory) {
+            return '';
+        }
         if (this.historyIndex === this.history.length - 1) {
             this.history[this.historyIndex] = this.buffer;
         }
@@ -91,24 +133,29 @@ class Shell {
             this.historyIndex--;
             this.buffer = this.history[this.historyIndex];
             this.cursor = this.buffer.length;
-            this.term.write(this.rewriteLine());
+            return this.rewriteInput();
         }
+        return '';
     }
 
     handleDownKey() {
+        if (!this.saveHistory) {
+            return '';
+        }
         if (this.historyIndex < this.history.length - 1) {
             this.historyIndex++;
             this.buffer = this.history[this.historyIndex];
             this.cursor = this.buffer.length;
-            this.term.write(this.rewriteLine());
+            return this.rewriteInput();
         } 
+        return '';
     }
 
     handleBackspaceKey() {
-        if(this.cursor===0) { return; }
+        if(this.cursor===0) { return ''; }
         this.buffer = this.buffer.slice(0, this.cursor - 1) + this.buffer.slice(this.cursor);
         this.cursor--;
-        this.term.write(this.rewriteLine(this.buffer.length + 1));
+        return this.rewriteInput();
     }
 
     cleanString(str) {
@@ -120,48 +167,103 @@ class Shell {
     }
 
     handleInput(data) {
-        if (data === '') { return; }
+        data = data.replace(/\r\n/g, '\r').replace(/\n/g, '\r');
         console.log([data]);
+        if (data === '\x03') { // CTRL+C
+            this.rejected = true;
+            if (this.rejecter) {
+                this.rejecter(new Error('killed'));
+                this.inputResolver = null;
+                this.rejecter = null;
+                this.rejected = false;
+            }
+        }
+        if (!this.inputResolver) {
+            return;
+        }
         if (data === '\r') { // ENTER
-            this.handleReturnKey();
+            this.term.write(this.handleReturnKey()+'\x1b[?25l', () => {
+                this.setCursorOffset();
+                if (this.inputResolver) {
+                    this.inputResolver(this.buffer);
+                    this.inputResolver = null;
+                    this.rejecter = null;
+                }
+                this.buffer = '';
+                this.cursor = 0;
+            });
             return;
         }
         if (data === '\x1b[D') { // LEFT
-            this.handleLeftKey();
-            return;
+            this.term.write(this.handleLeftKey());
         }
-        if (data === '\x1b[C') { // RIGHT
-            this.handleRightKey();
-            return;
+        else if (data === '\x1b[C') { // RIGHT
+            this.term.write(this.handleRightKey());
         }
-        if (data === '\x1b[A') { // UP
-            this.handleUpKey();
-            return;
+        else if (data === '\x1b[A') { // UP
+            this.term.write(this.handleUpKey());
         }
-        if (data === '\x1b[B') { // DOWN
-            this.handleDownKey();
-            return;
+        else if (data === '\x1b[B') { // DOWN
+            this.term.write(this.handleDownKey());
         }
-        if (data === '\x7f') { // BACKSPACE
-            this.handleBackspaceKey();
-            return;
+        else if (data === '\x7f') { // BACKSPACE
+            this.term.write(this.handleBackspaceKey());
         }
-        data = this.cleanString(data);
-        this.buffer = this.buffer.slice(0, this.cursor) + data + this.buffer.slice(this.cursor);
-        this.cursor += data.length;
-        this.term.write(this.rewriteLine());
+        else{
+            data = this.cleanString(data);
+            if (data.length !== 1) { return; }
+            this.buffer = this.buffer.slice(0, this.cursor) + data + this.buffer.slice(this.cursor);
+            this.cursor += data.length;
+            this.term.write(this.rewriteInput());
+        }
+    }
+}
+
+class Shell {
+    constructor() {
+        this.io = new IO();
+        this.cwd = '[0x0c5cbae8 ~]$ ';
+        this.io.print(this.cwd);
     }
 
-    runCommand(cmd) {
+    async run() {
+        while (true) {
+            try {
+                const input = await this.io.readLine(true);
+                await this.runCommand(input);
+            } catch {
+                await this.io.print('\r\n');
+            }
+            await this.io.print(this.cwd);
+        }
+    }
+
+    async runCommand(cmd) {
         cmd = cmd.trim();
-        if(cmd===''){
-            return '';
+        if (cmd === '') {
+            await this.io.print('');
+            return;
         }
-        if(cmd==='clear'){
-            return '\x1b[2J\x1b[0;0H';
+        if (cmd === 'clear') {
+            this.io.term.clear();
+            return;
         }
-        return `${cmd}: command not found\r\n`;
+        if (cmd === 'test') {
+            await this.io.print('Test command executed successfully.\r\n');
+            const input = await this.io.readLine();
+            await this.io.sleep(1000);
+            await this.io.print(`You entered: ${input}\r\n`);
+            await this.io.print('Test command executed successfully.\r\n');
+            return;
+        }
+        if(cmd === 'test2') {
+            while(true) {
+                await this.io.print("yes\r\n");
+            }
+        }
+        await this.io.print(`${cmd}: command not found\r\n`);
     }
 }
 
 const shell = new Shell();
+shell.run();
